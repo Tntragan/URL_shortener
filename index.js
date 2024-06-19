ef04b6958ae4f636c1ebd12974586305d059061a
Tntragan <tomandtammyragan@gmail.com>
2024-06-18 19:57:55 -0700

    Initial commit

--- index.js
+++ index.js
@@ -10,15 +10,20 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.listen(port, function() {
+app.get('/api/shorturl', (req, res) => {
+  res.json({ greeting: 'hello API' })
+  console.log(__dirname + req.path)
+})
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
