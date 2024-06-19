bc8a68270e92b04607ba7807e6dc9722e5c5831f
Tntragan <tomandtammyragan@gmail.com>
2024-06-19 14:41:58 -0700

    feat: make functional program

--- index.js
+++ index.js
@@ -2,11 +2,30 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mongoose = require('mongoose');
+const dns = require('dns');
+const urlparser = require('url');
+
+mongoose.connect(process.env.MONGO_URI);
+
+const urlSchema = new mongoose.Schema({
+  url: {
+    type: String,
+    required: true
+  },
+  short_url: {
+    type: String,
+    required: true
+  }
+});
+
+const Url = mongoose.model('url', urlSchema);
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -19,9 +38,33 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.get('/api/shorturl', (req, res) => {
-  res.json({ greeting: 'hello API' })
-  console.log(req)
+
+app.post('/api/shorturl', (req, res) => {
+  let originalUrl = req.body.url;
+  const dnslookup = dns.lookup(urlparser.parse(originalUrl).hostname,
+    async (err, address) => {
+      if (!address) {
+        res.json({ error: "Invalid URL" })
+      } else {
+        const urlCount = await Url.countDocuments({});
+        const urlObj = {
+          url: originalUrl,
+          short_url: urlCount
+        }
+        const result = await Url.create(urlObj);
+        console.log(result);
+        res.json({
+          original_url: originalUrl,
+          short_url: urlCount
+        })
+      }
+    })
+})
+
+app.get('/api/shorturl/:short_url', async (req, res) => {
+  const shorturl = req.params.short_url;
+  const urlObj = await Url.findOne({ short_url: +shorturl });
+  res.redirect(urlObj.url)
 })
 
 app.listen(port, function () {
